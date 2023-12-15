196afe60e11c7d8f9b06188cae36eb9eec3dc268
Mechi Bakker <93297328+MechiBakker@users.noreply.github.com>
2023-12-15 16:54:55 -0300

    movie

---
+++ movie-service/src/main/java/com/dh/movieservice/service/MovieService.java
@@ -0,0 +1,32 @@
+package com.dh.movieservice.service;
+
+import com.dh.movieservice.model.*;
+import com.dh.movieservice.repository.MovieRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author vaninagodoy
+ */
+
+@Service
+public class MovieService {
+
+    private final MovieRepository movieRepository;
+
+    public MovieService(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+    public List<Movie> getAll() {
+        return repository.findAll();
+    }/*MODIFICAR Y AGREGAR LO DEL ERROR*/
+
+    public List<Movie> findByGenre(String genre) {
+        return movieRepository.findByGenre(genre);
+    }
+
+    public Movie save(Movie movie) {
+        return movieRepository.save(movie);
+    }
+}
